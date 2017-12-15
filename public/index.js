9a431e05f614c09dee9b553d1dfada362fa4a549
Huascar <huascarmc@hotmail.com>
2017-12-15 11:59:08 +0000

    added makerequest function

--- public/index.js
+++ public/index.js
@@ -1,5 +1,18 @@
 var app = function(){
- 
+
 }
+
+const makeRequest = function(url, callback) {
+ const request = new XMLHttpRequest();
+
+ const url = 'https://restcountries.eu/rest/v2/all';
+
+ request.open('GET', url);
+ request.send();
+
+ request.addEventListener('load', callback);
+}
+
+
 
 document.addEventListener('DOMContentLoaded', app);
