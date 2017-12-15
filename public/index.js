bd213586b9a45c7305af6413031017484afd4224
Huascar <huascarmc@hotmail.com>
2017-12-15 12:00:14 +0000

    adeed requestcomplete function

--- public/index.js
+++ public/index.js
@@ -1,11 +1,10 @@
 var app = function(){
-
+ const url = 'https://restcountries.eu/rest/v2/all';
+ makeRequest(url, requestComplete);
 }
 
 const makeRequest = function(url, callback) {
  const request = new XMLHttpRequest();
-
- const url = 'https://restcountries.eu/rest/v2/all';
 
  request.open('GET', url);
  request.send();
@@ -13,6 +12,10 @@ const makeRequest = function(url, callback) {
  request.addEventListener('load', callback);
 }
 
-
+const requestComplete = function() {
+ if(this.status !== 200) return;
+const jsonString = this.responseText;
+const countries = JSON.parse(jsonString);
+}
 
 document.addEventListener('DOMContentLoaded', app);
